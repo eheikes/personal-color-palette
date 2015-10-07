0ce0b119adc5da6e69437fd62f0ae3202460c3ce
Eric Heikes <eheikes@gmail.com>
2015-10-06 22:39:22 -0500

    Replace Angular Material with Lumx.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
-  var app = angular.module('app', ['ngRoute', 'ngMaterial'])
+  var app = angular.module('app', ['ngRoute', 'lumx'])
   .config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
 
